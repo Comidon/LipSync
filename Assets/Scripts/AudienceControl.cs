29a8e565878f7197a69b6a1bc22f68aad0f7830e
Qing <qingqigao12@gmail.com>
2019-11-06 18:30:21 -0700

    shaking mechanic

---
+++ Assets/Scripts/AudienceControl.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudienceControl : MonoBehaviour
+{
+    private bool shaking = true;
+    private float shakeAmt = 5f;
+   
+
+    private void Update()
+    {
+        
+        if (shaking)
+        {
+            Vector3 newPos = transform.position + Random.insideUnitSphere * (Time.deltaTime * shakeAmt);
+            newPos.z = transform.position.z;
+            transform.position = newPos;
+          
+        }
+    }
+    public void ShakeMe()
+    {
+        StartCoroutine("ShakeNow");
+    }
+
+    IEnumerator ShakeNow()
+    {
+        Vector3 originalPos = transform.position;
+        if(shaking == false)
+        {
+            shaking = true;
+        }
+        yield return new WaitForSeconds(2.5f);
+
+        shaking = false;
+        transform.position = originalPos;
+            
+    }
+        
+}
