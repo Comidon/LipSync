dcd63124890a33f3f3617807a4f7947431562a2b
Don <comidon@outlook.com>
2019-11-07 19:04:15 -0700

    Fix

--- Assets/AudioVizAssets/Scripts/Instantiate512.cs
+++ Assets/AudioVizAssets/Scripts/Instantiate512.cs
@@ -5,19 +5,20 @@ using UnityEngine;
 public class Instantiate512 : MonoBehaviour
 {
     public GameObject _sampleCubePrefab;
-    GameObject[] _sampleCube = new GameObject[512];
+    GameObject[] _sampleCube = new GameObject[64];
     public float _maxScale;
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < 512; i++)
+        for (int i = 0; i < 64; i++)
         {
             GameObject _instanceSampleCube = (GameObject)Instantiate(_sampleCubePrefab);
             _instanceSampleCube.transform.position = this.transform.position;
+            _instanceSampleCube.transform.rotation = this.transform.rotation;
             _instanceSampleCube.transform.parent = this.transform;
             _instanceSampleCube.name = "SampleCube" + i;
-            this.transform.eulerAngles = new Vector3(0, -0.703125f * i, 0);
-            _instanceSampleCube.transform.position = Vector3.forward * 100;
+            this.transform.localEulerAngles = new Vector3(0, -0.703125f * i * 8, 0);
+            _instanceSampleCube.transform.position = Vector3.forward;
             _sampleCube[i] = _instanceSampleCube;
           
       
@@ -27,11 +28,11 @@ public class Instantiate512 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        for(int i = 0; i < 512; i++)
+        for(int i = 0; i < 64; i++)
         {
             if(_sampleCube != null)
             {
-                _sampleCube[i].transform.localScale = new Vector3(10, (AudioVisual._samples[i] * _maxScale) + 2, 10);
+                _sampleCube[i].transform.localScale = new Vector3(2.5f, (AudioVisual._samples[i] * _maxScale) / 8 + 0.25f, 2.5f);
             }
         }
     }
