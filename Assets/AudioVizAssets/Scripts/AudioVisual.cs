91fc55ef122b05f64706587b95c21e9bc958b7a1
Don <comidon@outlook.com>
2019-11-13 19:32:12 -0700

    Revert "Merge"
    This reverts commit c4d2a80fc8f047df9068026ff8484432199a36dd, reversing
    changes made to 1ce372776635f27ce4d591c588eef413e7605352.

--- Assets/AudioVizAssets/Scripts/AudioVisual.cs
+++ Assets/AudioVizAssets/Scripts/AudioVisual.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class AudioVisual : MonoBehaviour
 {
     AudioSource _audioSource;
-    public static float[] _samples = new float[512];
+    public static float[] _samples = new float[64];
     // Start is called before the first frame update
     void Start()
     {
--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -4,8 +4,7 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
-    [SerializeField]
-    private float thatsGood = 0.149f;
+    private float thatsGood = 0.2f;
 
     private float damage = 10f;
 
@@ -43,43 +42,24 @@ public class PlayerManager : MonoBehaviour
         playerYPos = Input.GetAxis("Vertical") * 2;
         transform.position = new Vector3(0, playerYPos, 0);
         */
-<<<<<<< HEAD
         //playerYPos = van_sama.GetLipsPosition().y;
         /*playerYPos = Input.GetAxis("Vertical") * 0.45f + 0.8f;*/
-=======
-        playerYPos = van_sama.GetLipsPosition().y;
->>>>>>> Yuhan
     }
 
     private void FixedUpdate()
     {
         Debug.Log(hp);
-<<<<<<< HEAD
         /*Debug.Log("Line pos: " + SongDrawer.instance.GetOriginYPos());
         Debug.Log("Player pos: " + playerYPos);*/
         /*if (Mathf.Abs(SongDrawer.instance.GetOriginYPos() - playerYPos) > thatsGood)
-=======
-        //Debug.Log("Line pos: " + DrawWave.instance.GetOriginYPos());
-        //Debug.Log("Player pos: " + playerYPos);
-        if (Mathf.Abs(DrawWave.instance.GetOriginYPos() - playerYPos) > thatsGood)
->>>>>>> Yuhan
         {
             hp -= damage;
-        }
-
-        if (hp < 99.8)
-        {
-            hp += recover;
         }
         else
         {*/
         if (hp < 99.8)
         {
-<<<<<<< HEAD
             hp += recover;
-=======
-            hp = 100;
->>>>>>> Yuhan
         }
         else
         {
